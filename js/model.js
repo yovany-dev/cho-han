84bb74645637ce3d6a6281bbcc37a50a4dcf6547
Yovani Morales <yovanymorales.priv@gmail.com>
2023-04-26 12:54:38 -0400

    Added methods to store and read data in local storage.

--- js/model.js
+++ js/model.js
@@ -1,10 +1,19 @@
 export default class Model {
     constructor() {
         this.view = null;
+        this.localStorage = window.localStorage;
     }
 
     setView(view) {
         this.view = view;
+    }
+
+    saveData(key, value) {
+        this.localStorage.setItem(key, value);
+    }
+
+    readData(item) {
+        return this.localStorage.getItem(item);
     }
 
     isNewUser() {
